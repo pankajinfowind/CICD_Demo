dfcf098b49376820141bd707d8d1a062c32883d5
pankajinfowind <pankaj.infowind@gmail.com>
2020-10-08 13:36:56 +0530

    First Commit

---
+++ CICD_DemoTests/CICD_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  CICD_DemoTests.swift
+//  CICD_DemoTests
+//
+//  Created by mac on 07/10/20.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import XCTest
+@testable import CICD_Demo
+
+class CICD_DemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
